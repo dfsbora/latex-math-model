59ce275b78e22d628c946cae3d86466e3b24ba84
dfsbora <dfsbora@gmail.com>
2024-06-19 21:40:58 +0200

    parse label and cite in given text and compare to list of labels and cites of the original dataset

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import re
-
+from collections import Counter
 def generate_config_dict(arguments, architecture_name='LSTM'):
     """
     Generate config dictionary for wandb
@@ -14,6 +14,8 @@ def generate_config_dict(arguments, architecture_name='LSTM'):
     dict = {key: dict[key] for key in keys}
     dict['architecture'] = architecture_name
     return dict
+
+
 
 def correct_latex_environments(text):
     begin_pattern = r'\\begin\{([^\}]+)\}'
@@ -68,3 +70,87 @@ def correct_latex_environments(text):
 
     return '\n'.join(output_lines), num_corrected_env, num_closed_env, num_deleted_end
 
+
+
+
+class EvaluatePrompt:
+    def __init__(self, text, cites_file = "data/cites.txt", labels_file = "data/labels.txt"):
+        self.text = text
+        #TODO add comparison of formulas
+        #TODO add comparison of refs to labels in prompt and in original dataset
+        self.cites_list = self._file_to_content(cites_file)
+        self.labels_list = self._file_to_content(labels_file)
+
+        #self.pattern_single = re.compile(r'(?<!\\)\$(?!\$)(.*?)(?<!\\)\$(?!\$)')
+        #self.pattern_double = re.compile(r'(?<!\\)\$\$(.*?)(?<!\\)\$\$', re.DOTALL)
+        self.cite_pattern = re.compile(r'\\cite\{(.*?)\}')
+        self.label_pattern = re.compile(r'\\label\{(.*?)\}')
+
+    def set_text(self, new_text):
+        self.text = new_text
+
+    def _file_to_content(self, input_file):
+        with open(input_file, 'r', encoding='utf-8') as file:
+            content = set(line.strip() for line in file)
+        return content
+
+    def _parse_formulas(self):
+        double_matches = self.pattern_double.findall(self.text)
+        single_matches = self.pattern_single.findall(self.text)
+        all_formulas = single_matches + double_matches
+        return all_formulas
+
+    def _parse_refs(self):
+        matches = self.ref_pattern.findall(self.text)
+        return matches
+
+    def _parse_cites(self):
+        matches = self.cite_pattern.findall(self.text)
+        return matches
+
+    def _parse_labels(self):
+        matches = self.label_pattern.findall(self.text)
+        return matches
+
+
+    def compare_labels_with_original_labels(self):
+        # Find labels in the outputted text
+        prompt_labels = self._parse_labels()
+        # Compare labels with list of original dataset
+        common = set(prompt_labels) & self.labels_list
+
+        num_generated = len(set(prompt_labels))
+        num_copy = len(common)
+        num_original = num_generated - num_copy
+        return num_generated, num_copy, num_original
+
+
+    def compare_cites_with_original_cites(self):
+        # Find labels in the outputted text
+        prompt_cites = self._parse_cites()
+        # Compare labels with list of original dataset
+        common = set(prompt_cites) & self.cites_list
+
+        num_generated = len(set(prompt_cites))
+        num_copy = len(common)
+        num_original = num_generated - num_copy
+        return num_generated, num_copy, num_original
+
+
+
+
+
+if __name__ == "__main__":
+    input_text = """
+    This is a sample text with some formulas. Here is one: $a^2 + b^2 = c^2$ \cite{dag12} and here is another: $$E=mc^2$$ \cite{cite4}.
+    Another inline formula is $k^* = Lk^*$.
+    """
+
+    # Instantiate the class with the input text
+    evaluator = EvaluatePrompt(input_text)
+
+    # Compare parsed formulas with another text file
+    num_generated, num_copy, num_original = evaluator.compare_cites_with_original_cites()
+    print(num_generated)
+    print(num_copy)
+    print(num_original)
