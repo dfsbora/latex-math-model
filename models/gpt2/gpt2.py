67627468e070888538e90702527f4f52aaa39200
Rico Bruland <ricobruland@gmail.com>
2024-08-30 14:40:12 +0200

    Update gpt2.py

--- models/gpt2/gpt2.py
+++ models/gpt2/gpt2.py
@@ -1,7 +1,7 @@
 import os
 import torch
 import wandb
-import argpase
+import argparse
 from torch.utils.data import Dataset, random_split
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments, DataCollatorForLanguageModeling, TrainerCallback
 from models.utils.evaluate_metrics import (
